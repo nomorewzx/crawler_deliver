84eba1118d74220f4851fae3ee5a280aa2f5b112
nomorewzx <nomorewzx@163.com>
2019-10-29 23:26:38 +0800

    continue if fail

--- crawler.py
+++ crawler.py
@@ -14,6 +14,12 @@ def update_visited_url(url: str):
     with open(settings.VISITED_URL_FILES, 'a') as f:
         f.write('{}\n'.format(url))
 
+
+def update_can_not_download_url(url: str):
+    with open(settings.CANNOT_DOWNLOAD_FILES, 'a') as f:
+        f.write('{}\n'.format(url))
+
+
 def download_document(document_url: str):
     print('find document to download: {}'.format(document_url))
     url_path_parse_result  = parse.urlsplit(document_url)
@@ -29,13 +35,23 @@ def download_document(document_url: str):
 
     print('downloading...')
     start = time.time()
-    response = requests.get(document_url)
-    with open(file_path, 'wb') as fd:
-        fd.write(response.content)
+    for i in range(settings.DOWNLOAD_MAX_TRY):
+        try:
+            response = requests.get(document_url)
+            with open(file_path, 'wb') as fd:
+                fd.write(response.content)
+            end = time.time()
+            print('download file into: {}, takes {} s'.format(file_path, end - start))
+            update_visited_url(document_url)
+            return
+        except Exception as ex:
+            if i < settings.DOWNLOAD_MAX_TRY:
+                print('error happens when downloading, try again')
+                continue
+    update_can_not_download_url(document_url)
+    print('can not download: {}, continue to download others'.format(document_url))
+    return
 
-    end = time.time()
-    print('download file into: {}, takes {} s'.format(file_path, end - start))
-    update_visited_url(document_url)
 
 def crawl_url(start_url):
     if start_url in VISITED_URLS:
@@ -67,17 +83,26 @@ def crawl_url(start_url):
 
 
 def load_visited_urls():
-    if not os.path.exists(settings.VISITED_URL_FILES):
+    if not os.path.exists(settings.VISITED_URL_FILES) and not os.path.exists(settings.CANNOT_DOWNLOAD_FILES):
         return
 
     with open(settings.VISITED_URL_FILES, 'r') as f:
         for line in f.readlines():
             VISITED_URLS.append(line.strip())
 
+    with open(settings.CANNOT_DOWNLOAD_FILES, 'r') as f:
+        for line in f.readlines():
+            VISITED_URLS.append(line.strip())
+
 
 def main():
-    load_visited_urls()
-    crawl_url(settings.START_PAGE_URL)
+    while True:
+        try:
+            load_visited_urls()
+            crawl_url(settings.START_PAGE_URL)
+        except Exception as ex:
+            print('error happens: {}'.format(ex))
+            continue
 
 
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -3,4 +3,6 @@ import os
 START_PAGE_URL = 'https://www.etsi.org/deliver/'
 HOST = 'https://www.etsi.org'
 VISITED_URL_FILES = 'visited_url.txt'
+CANNOT_DOWNLOAD_FILES = 'cannot_download.txt'
 PROJECT_BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+DOWNLOAD_MAX_TRY = 3
